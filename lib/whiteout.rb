ef406b50ce81bc1bc8b2492ee458e53b9bc61f09
Cameron Desautels <camdez@gmail.com>
2012-02-13 02:04:52 -0600

    Added basic version of command line utility that reads from standard input (only)

--- lib/whiteout.rb
+++ lib/whiteout.rb
@@ -1,6 +1,27 @@
 require "whiteout/version"
+require "optparse"
 
 module Whiteout
+  def self.execute(*args)
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: whiteout file1 [...]"
+
+      opts.on('-h', '--help', 'Display this screen' ) do
+        puts opts
+        exit
+      end
+    end
+
+    begin
+      opts.parse!
+
+      puts self.clean($stdin.read)
+    rescue OptionParser::InvalidOption => e
+      warn e
+      puts opts
+    end
+  end
+
   def self.clean(str)
     out = ""
     str.each_line do |line|
